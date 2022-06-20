141c280520414e525b51239a00b97df31c07a2a5
Nikita Khavronin <khavrks@gmail.com>
2022-06-19 20:50:10 -0400

    s

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  rules: {
+    'react-native/no-color-literals': 'off',
+    'react-native/no-raw-text': 'off',
+    'prettier/prettier': ['error', {endOfLine: 'auto'}],
+  },
 };
